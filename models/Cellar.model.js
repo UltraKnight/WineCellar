0c1249ac28e8bcb764a4abb3cd1b9f3ee5855847
Vanderlei I Martins <vanderlei.i.martins@outlook.com>
2021-02-14 16:04:37 +0000

    changes in Cellar model

--- models/Cellar.model.js
+++ models/Cellar.model.js
@@ -3,11 +3,21 @@ const { Schema, model } = mongoose;
 
 const cellarSchema = new Schema ({
 
-    name: String,
+    name: {
+        type: String,
+        required: [true, 'You need to give a name to your cellar.'],
+        unique: true
+    },
     place: String,
-    capacity: String,
-    wines: [String]
-
+    capacity: {
+        Number,
+        max: 900,
+        default: 30
+    },
+    wines: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Wine' //Relates to the Wine model
+    }]
 });
 
 module.exports = model('Cellar', cellarSchema);
