884b356c5ec40c1524917e5e05edab85942bd662
Vanderlei I Martins <vanderlei.i.martins@outlook.com>
2021-02-14 12:06:53 +0000

    add effect to title

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-  console.log('IronGenerator JS imported successfully!');
-
+  //console.log('IronGenerator JS imported successfully!');
 }, false);
+
+window.onload = () => {
+  const appNameElem = document.getElementById('app-name');
+  appNameElem.classList.add('big-text');
+
+
+  const updateScroll = () => {
+    if(window.scrollY <= 10) {
+      appNameElem.classList.add('big-text');
+    } else {
+      appNameElem.classList.remove('big-text');
+    }
+  };
+  
+  window.addEventListener('scroll', updateScroll);
+};
