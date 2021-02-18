bd36155247064307813e03b7322c70977aa47f95
Vanderlei I Martins <vanderlei.i.martins@outlook.com>
2021-02-18 16:06:48 +0000

    user model new fields

--- models/User.model.js
+++ models/User.model.js
@@ -22,8 +22,50 @@ const userSchema = new Schema ({
         require: [true, 'Password is required']
       },
       
-      imageURL: String
+      imageURL: String,
+
+      createdWines: {
+        red: {
+          type: Number,
+          default: 0
+        },
+        white: {
+          type: Number,
+          default: 0
+        },
+        rose: {
+          type: Number,
+          default: 0
+        },
+        sparkling: {
+          type: Number,
+          default: 0
+        },
+        dessert: {
+          type: Number,
+          default: 0
+        },
+        green: {
+          type: Number,
+          default: 0
+        },
+        porto: {
+          type: Number,
+          default: 0
+        },
+        total: {
+          type: Number,
+          default: 0
+        },
+      },
+      createdCellars: {
+        type: Number,
+        default: 0
+      },
+      picChanged: {
+        type: Boolean,
+        default: false
+      }
   });
 
-      
-     module.exports = model('User', userSchema);
+module.exports = model('User', userSchema);
